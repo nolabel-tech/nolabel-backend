698174ac779ebe607e910701326b79bcdaa3d5cc
Secure Messages <doweaya@gmail.com>
2024-06-29 00:21:54 +0300

    cors: [main] cors allowed

--- sm2/settings.py
+++ sm2/settings.py
@@ -51,10 +51,8 @@ CORS_ALLOW_ALL_HEADERS = True
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+    "https://app.sm2.fun",
 ]
-
 
 
 
